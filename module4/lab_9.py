b132173e9fe747a7ca9f89fc28408944b6ccea0f
Eugeny Konstantinov <jskonst@yandex.ru>
2023-02-11 14:31:54 +0300

    Added sample

---
+++ module4/lab_9.py
@@ -0,0 +1,15 @@
+import math
+def first(x):
+    p = 1
+    for x_i in x:
+        p *= (2+ math.cos(x_i**2))
+    print(f"P={p}")
+
+def second(z):
+    s = 0
+    for z_i in z:
+        s += math.exp(-2*z_i)*math.log(abs(z_i))
+    print(f"S={s}")
+
+first([1,2,3,4,5])
+second([ 0.1, 0.2,0.3, 0.4,  0.5])
